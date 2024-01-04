4a625fb6f703a56e4c8342b0ddab0502865ae1b3
Claus Bube <claus@kalna.it>
2024-01-04 13:55:37 +0100

    Make dkMitIDCompletionData attributes optional
    The properties of the dkMitIDCompletionData class have been modified to be optionally nullable. This change introduces more flexibility in the data model, allowing for instances where some of the information may not be readily available or necessary.

--- src/Resources/CompletionData/dkMitIDCompletionData.php
+++ src/Resources/CompletionData/dkMitIDCompletionData.php
@@ -7,10 +7,10 @@ use Carbon\Carbon;
 class dkMitIDCompletionData
 {
     public string $providerName;
-    public string $cpr;
-    public Carbon $dateOfBirth;
-    public string $employeeData;
-    public string $ial;
-    public string $identityName;
+    public ?string $cpr;
+    public ?Carbon $dateOfBirth;
+    public ?string $employeeData;
+    public ?string $ial;
+    public ?string $identityName;
     public string $userId;
 }
