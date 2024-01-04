21003ff4b390a4be9df445cc01125213b4d5c643
Claus Bube <claus@kalna.it>
2024-01-04 16:26:46 +0100

    Refactor NewScriveSignInEvent constructor
    Removed the public payload property from NewScriveSignInEvent constructor and replaced it with a strongly typed CompletionData argument. This change enhances type safety and makes clear we are expecting a CompletionData instance, providing readability and better self-documentation.

--- src/Events/NewScriveSignInEvent.php
+++ src/Events/NewScriveSignInEvent.php
@@ -5,15 +5,13 @@ namespace KalnaLab\Scrive\Events;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use KalnaLab\Scrive\Resources\CompletionData\CompletionData;
 
 class NewScriveSignInEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $payload;
-
-    public function __construct($payload)
+    public function __construct(public CompletionData $payload)
     {
-        $this->payload = $payload;
     }
 }
