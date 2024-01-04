13f14469d385c58925e99b8255d076c80a2cca46
Claus Bube <claus@kalna.it>
2024-01-04 11:10:20 +0100

    Add `dkMitID` method to ScriveController
    Included `dkMitID` method in the `ScriveController` class to handle authorization with Danish MitID provider. By encapsulating authorization inside ScriveController, the code is more maintainable and potential bugs associated with endpoint construction are reduced.

--- src/Http/Controllers/ScriveController.php
+++ src/Http/Controllers/ScriveController.php
@@ -4,10 +4,17 @@ namespace KalnaLab\Scrive\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use KalnaLab\Scrive\Resources\AuthProviders\dkMitID;
 use KalnaLab\Scrive\Scrive;
 
 class ScriveController extends Controller
 {
+    public function dkMitID()
+    {
+        $provider = new dkMitID();
+        return Scrive::authorize($provider);
+    }
+
     public function authenticate(Request $request)
     {
         $transactionId = $request->get('transaction_id');
