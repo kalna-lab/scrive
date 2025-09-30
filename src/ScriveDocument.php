95cfae83f26d6424db389554d58151ecb16341aa
Claus Hjort Bube <chb@kalna.dk>
2025-09-30 13:59:00 +0200

    Refactor `ScriveDocument` to include `update` functionality, adjust API call logic, and streamline header and payload handling.

--- src/ScriveDocument.php
+++ src/ScriveDocument.php
@@ -2,16 +2,12 @@
 
 namespace KalnaLab\Scrive;
 
-use KalnaLab\Scrive\Events\NewScriveSignInEvent;
-use KalnaLab\Scrive\Resources\AuthProviders\Provider;
+use Illuminate\Support\Arr;
 
 class ScriveDocument
 {
-    const METHOD = [
-        'AUTH' => 'auth',
-        'SIGN' => 'sign',
-    ];
-
+    public string $documentId;
+    public object $documentJson;
     public string $httpMethod = 'POST';
     public string $env = 'live';
     public array $headers = [];
@@ -28,32 +24,104 @@ class ScriveDocument
     /**
      * @throws \Exception
      */
-    public function newFromTemplate(string $documentId): bool
+    public function newFromTemplate(string $documentId): string
     {
-        $this->endpoint .= $documentId;
-        $this->httpMethod = 'GET';
+        $this->endpoint .= 'newfromtemplate/' . $documentId;
+        $this->httpMethod = 'POST';
 
         $this->instantiateCurl();
 
         $payload = $this->executeCall();
 
-        $provider = Provider::parse($payload);
-        if ($provider->success) {
-            $provider->setTransactionId($transactionId);
-            NewScriveSignInEvent::dispatch($provider->completionData);
+        if (is_object($payload) && property_exists($payload, 'id')) {
+            $this->documentJson = $payload;
+            $this->documentId = $this->documentJson->id;
         }
 
-        return $provider->success;
+        return $this->documentId;
     }
 
-    public function sign()
+    public function update(array|string $name, array $values = []): void
     {
+        $this->endpoint .= '/' . $this->documentId . '/update';
+        $this->httpMethod = 'POST';
+
+        $firstName = '';
+        $lastName = '';
+        $fullName = $name;
+        if (is_array($name)) {
+            $fullName = implode(' ', $name);
+        }
+        if (is_string($name)) {
+            $name = explode(' ', $name);
+        }
+        if (is_array($name)) {
+            $firstName = Arr::first($name);
+            $lastName = Arr::last($name);
+        }
+
+        $documentJson = $this->documentJson;
+
+        foreach ($documentJson->parties as $pIdx => $party) {
+            if ($party->signatory_role == 'signing_party') {
+                foreach ($party->fields as $fIdx => $field) {
+                    if ($field->type == 'name' && $field->order == 1) {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $firstName;
+
+                        continue;
+                    }
+                    if ($field->type == 'name' && $field->order == 2) {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $lastName;
+
+                        continue;
+                    }
+                    if ($field->type == 'full_name') {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $fullName;
+
+                        continue;
+                    }
+                    if ($field->type == 'email') {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $values['email'] ?? '';
+
+                        continue;
+                    }
+                    if ($field->type == 'personal_number') {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $values['personal_number'] ?? $values['cpr'] ?? '';
+
+                        continue;
+                    }
+                    if ($field->type == 'company_number') {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $values['company_number'] ?? $values['cvr'] ?? '';
+
+                        continue;
+                    }
+                    if (array_key_exists($field->name, $values)) {
+                        $documentJson->parties[$pIdx]->fields[$fIdx]->value = $values[$field->name];
+                    }
+                }
+                $documentJson->parties[$pIdx]->delivery_method = 'api';
+            }
+        }
+
+        $documentJson->api_callback_url = ''; //TODO: set callback url
+        $this->body['document'] = json_encode($documentJson);
+
+        $this->instantiateCurl();
+
+        $payload = $this->executeCall();
+
+        if (is_object($payload) && property_exists($payload, 'id')) {
+            $this->documentJson = $payload;
+        }
     }
 
     public function instantiateCurl(): void
     {
         $this->headers = [
-            'Authorization' => 'Bearer ' . config('scrive.' . $this->env . '.token'),
+            'Authorization' => 'oauth_signature_method="PLAINTEXT",' .
+                'oauth_consumer_key="' . config('scrive.document.' . $this->env . '.api-token') . '",' .
+                'oauth_token="' . config('scrive.document.' . $this->env . '.access-token') . '",' .
+                'oauth_signature="' . config('scrive.document.' . $this->env . '.api-secret') . '&' . config('scrive.document.' . $this->env . '.access-secret') . '"',
         ];
 
         $this->curlObject = curl_init();
@@ -66,13 +134,9 @@ class ScriveDocument
         curl_setopt($this->curlObject, CURLOPT_TIMEOUT, 30);
     }
 
-    public function setHeaders(array $body = []): void
+    public function setHeaders(): void
     {
         $headers = [];
-        if ($body) {
-            $this->headers['Content-length'] = strlen(json_encode($body, JSON_UNESCAPED_SLASHES));
-            $this->headers['Content-Type'] = 'application/json';
-        }
         foreach ($this->headers as $key => $value) {
             $headers[] = $key . ': ' . $value;
         }
@@ -81,13 +145,14 @@ class ScriveDocument
 
     public function executeCall(): array|object
     {
-        $this->setHeaders($this->body);
+        $postFields = http_build_query($this->body);
+        $this->setHeaders();
         curl_setopt($this->curlObject, CURLOPT_URL, $this->endpoint);
         if ($this->httpMethod === 'GET') {
             curl_setopt($this->curlObject, CURLOPT_CUSTOMREQUEST, $this->httpMethod);
         } elseif (in_array($this->httpMethod, ['POST', 'PATCH'])) {
             curl_setopt($this->curlObject, CURLOPT_CUSTOMREQUEST, $this->httpMethod);
-            curl_setopt($this->curlObject, CURLOPT_POSTFIELDS, json_encode($this->body, JSON_UNESCAPED_SLASHES));
+            curl_setopt($this->curlObject, CURLOPT_POSTFIELDS, $postFields);
         }
         curl_setopt($this->curlObject, CURLOPT_VERBOSE, true);
 
